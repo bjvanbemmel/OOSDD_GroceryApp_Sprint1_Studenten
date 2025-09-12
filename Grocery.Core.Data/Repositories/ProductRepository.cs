22d7b41d2a5817917848a26daa5e9d1765c76ca6
Beau Jean van Bemmel <beau.vanbemmel@rysst.nl>
2025-09-12 16:43:23 +0200

    ProductRepository now stores and displays all four Products

--- Grocery.Core.Data/Repositories/ProductRepository.cs
+++ Grocery.Core.Data/Repositories/ProductRepository.cs
@@ -5,27 +5,26 @@ namespace Grocery.Core.Data.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly List<Product> products;
+        private List<Product> Products { get; set; };
         public ProductRepository()
         {
-            products = [
-                /* 
-                 * initieer de lijst met 4 nieuwe producten: 
-                 * Melk[voorraad 300], 
-                 * Kaas[voorraad 100], 
-                 * Brood[voorraad 400] en 
-                 * Cornflakes[voorraad 0]
-                */
-				];
+            Products = new List<Product>()
+            {
+                new Product(0, "Melk", 300),
+                new Product(1, "Kaas", 100),
+                new Product(2, "Brood", 400),
+                new Product(3, "Cornflakes", 0)
+            };
         }
+
         public List<Product> GetAll()
         {
-            return []; //wijzig dit in de gemaakte lijst uit de constructo
+            return Products;
         }
 
         public Product? Get(int id)
         {
-            return products.FirstOrDefault(p => p.Id == id);
+            return Products.FirstOrDefault(p => p.Id == id);
         }
 
         public Product Add(Product item)
--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -2,9 +2,13 @@
 {
     public class Product : Model
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
         public int Stock { get; set; }
         public Product(int id, string name, int stock) : base(id, name)
         {
+            Id = id;
+            Name = name;
             Stock = stock;
         }
     }
